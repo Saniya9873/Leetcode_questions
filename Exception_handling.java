8e37a73982e53a6ebdec8311465ae19654f4235e
Saniya Gupta <136055725+Saniya9873@users.noreply.github.com>
2025-01-07 21:10:35 +0530

    Update Exception_handling.java

--- Exception_handling.java
+++ Exception_handling.java
@@ -1,60 +1,60 @@
 //import java.io.IOException;
 public class Exception_handling {
     // TRY , CATRCH  AND FINALLY  BLOCK :
-    // public static void main(String[] args) {
-    //    try{
-    //     int a[]=new int[5];
-    //     a[5]=30/0;
-    //    }
-    //    catch(ArithmeticException a){
-    //     System.out.println("task1 is completed");
-    //    } 
-    //    catch(ArrayIndexOutOfBoundsException b){
-    //     System.out.println("task2 is completed");
-    //    }
-    //    finally{
-    //     System.out.println("finally block");
-    //    }
-    // }
+    public static void main(String[] args) {
+       try{
+        int a[]=new int[5];
+        a[5]=30/0;
+       }
+       catch(ArithmeticException a){
+        System.out.println("task1 is completed");
+       } 
+       catch(ArrayIndexOutOfBoundsException b){
+        System.out.println("task2 is completed");
+       }
+       finally{
+        System.out.println("finally block");
+       }
+    }
 
 
     //THROW  KEYWORD :
-    // static void validate(int age){
-    //     if(age<18){
-    //         throw new ArithmeticException("not valid");
-    //     }
-    //     else{
-    //         System.out.println("welcome to vote");
-    //     }
-    // }
-    // public static void main(String[] args) {
-    //     validate(13);
-    //     System.out.println("rest of code");
-    // }
+    static void validate(int age){
+        if(age<18){
+            throw new ArithmeticException("not valid");
+        }
+        else{
+            System.out.println("welcome to vote");
+        }
+    }
+    public static void main(String[] args) {
+        validate(13);
+        System.out.println("rest of code");
+    }
 
 
     //Throws  KEYWORD :
         // SYNTAX:
         //     return_type methodname() throws exception_classname{}
 
-    // void m() throws IOException{
-    //     throw new IOException("device error");
-    // }
-    // void n() throws IOException{
-    //     m();
-    // }
-    // void p(){
-    //     try{
-    //         n();
-    //     }catch(Exception e){
-    //         System.out.println("exception handled");
-    //     }
-    // }
-    // public static void main(String[] args) {
-    //     Exception_handling obj=new Exception_handling();
-    //     obj.p();
-    //     System.out.println("normal flow");
-    // }
+    void m() throws IOException{
+        throw new IOException("device error");
+    }
+    void n() throws IOException{
+        m();
+    }
+    void p(){
+        try{
+            n();
+        }catch(Exception e){
+            System.out.println("exception handled");
+        }
+    }
+    public static void main(String[] args) {
+        Exception_handling obj=new Exception_handling();
+        obj.p();
+        System.out.println("normal flow");
+    }
 
 
     //CUSTOM  EXCEPTION :
